fdbea6ac12165b9f64bd89c7d770ef01939f707f
Syed Asef Hussain <81021961+Syed-Asef@users.noreply.github.com>
2023-08-31 02:39:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ latitude =
 longitude =
 
 #API information request
-response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&forecast_days=1")
+response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&forecast_days=1").json()
+print(response)
