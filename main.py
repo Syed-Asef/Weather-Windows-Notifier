d5be373d2388ffa06b5019e4fc52cfc883a8a790
Syed Asef Hussain <81021961+Syed-Asef@users.noreply.github.com>
2023-08-31 21:14:36 +0100

    Notifications + Fixes
    Added the windows notification fully configured and fixed the min max temp + added emojis for the weather code.

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import requests
+from winotify import Notification
 
 #Change these variables to match your location
 latitude = 
 longitude =
 
+# Put this in a separate file
+weatherCodeIDs = {
+    0:"Clear sky ☀️",
+    1:"Clear ☀️", 2:"Partly cloudy ⛅", 3:"Overcast ☁️",
+    45:"Fog 🌫️", 48:"Fog 🌫️",
+    51:"Drizzle 🌧️", 53:"Drizzle 🌧️", 55:"Drizzle 🌧️", 56: "Freezing drizzle 🌧️", 57:"Freezing drizzle 🌧️",
+    61:"Slight rain 🌧️", 63:"Moderate rain 🌧️", 65:"Heavy rain 🌧️",
+    66:"Freezing rain 🌧️", 67:"Freezing rain 🌧️",
+    71:"Light snow fall 🌨️", 73:"Moderate snow fall 🌨️", 75:"Heavy snow fall 🌨️", 77:"Snow 🌨️",
+    80:"Light showers 🌧️", 81:"Moderate showers 🌧️", 82:"Violent showers 🌧️",
+    85:"Light snow showers 🌨️", 86:"Heavy snow showers 🌨️",
+    95:"Thunderstorm ⛈️", 96:"Thunderstorm + Light hail ⛈️", 99:"Thunderstorm + Heavy hail ⛈️"
+}
+
 #API information request + variable setting
 response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&forecast_days=1").json()
 currentTemp = response["current_weather"]["temperature"]
 weatherCode = response["current_weather"]["weathercode"]
-maxTemp = response["daily"]["temperature_2m_max"]
-minTemp = response["daily"]["temperature_2m_min"]
+maxTemp = response["daily"]["temperature_2m_max"][0]
+minTemp = response["daily"]["temperature_2m_min"][0]
 
+toast = Notification(app_id="Weather",
+                     title=f"Temperature: {currentTemp}°C\n{weatherCodeIDs[weatherCode]}",
+                     msg=f"Min/Max Temp: {minTemp}/{maxTemp}°C",
+                     duration="long")
 
-# Put this in a separate file
-weatherCodeIDs = {
-    0:"Clear sky",
-    1:"Clear", 2:"Partly cloudy", 3:"Overcast",
-    45:"Fog", 48:"Fog",
-    51:"Drizzle", 53:"Drizzle", 55:"Drizzle", 56: "Freezing drizzle", 57:"Freezing drizzle",
-    61:"Slight rain", 63:"Moderate rain", 65:"Heavy rain",
-    66:"Freezing rain", 67:"Freezing rain",
-    71:"Light snow fall", 73:"Moderate snow fall", 75:"Heavy snow fall", 77:"Snow",
-    80:"Light showers", 81:"Moderate showers", 82:"Violent showers",
-    85:"Light snow showers", 86:"Heavy snow showers",
-    95:"Thunderstorm", 96:"Thunderstorm + Light hail", 99:"Thunderstorm + Heavy hail"
-}
-
-print(weatherCodeIDs[weatherCode])
+toast.show()
